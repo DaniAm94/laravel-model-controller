7c060bff67d69e5124513ed16336d7a5bddc6eda
DaniAm94 <dani.amico94@gmail.com>
2024-03-11 15:55:49 +0100

    handled movies fetch into home's controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
+// Importo la classe Movie
+use App\Models\Movie;
+
 class HomeController extends Controller
 {
     //
     public function __invoke()
     {
-        return view('home');
+        // Restituisce tutte le istanze della tabella movies
+        $movies = Movie::all();
+        return view('home', compact('movies'));
     }
 }
