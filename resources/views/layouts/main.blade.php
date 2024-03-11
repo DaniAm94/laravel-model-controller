e71e460b45c74053631bc92d4468a1f5c327ad36
DaniAm94 <dani.amico94@gmail.com>
2024-03-11 15:27:43 +0100

    added dark theme

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 
 <head>
     <meta charset="UTF-8">
@@ -12,8 +12,10 @@
 
 <body>
     {{-- Header --}}
-    <header>
-        <h1>Laravel Model & Controller | MOVIE</h1>
+    <header class="py-3">
+        <div class="container-sm">
+            <h1 class="text-center">Laravel Model & Controller | MOVIE</h1>
+        </div>
     </header>
 
     {{-- Main --}}
