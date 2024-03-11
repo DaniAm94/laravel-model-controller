ddd33006bab4e202674aeb5e3db66d29d1e08fcd
DaniAm94 <dani.amico94@gmail.com>
2024-03-11 15:56:48 +0100

    added a template to show all movies' info

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,21 @@
 @section('title', 'Home')
 
 @section('main-content')
+    <div class="container-sm">
 
+        <div class="row row-gap-3 ">
+            @isset($movies)
+                @forelse ($movies as $movie)
+                    <div class="col-3">
+
+                        @include('includes.movies.card')
+                    </div>
+                @empty
+                    <div class="col-12">
+                        <h2>Non ci sono film</h2>
+                    </div>
+                @endforelse
+            @endisset
+        </div>
+    </div>
 @endsection
